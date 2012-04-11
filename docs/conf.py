8654db479b774dceb5338bcfed9232e38b6a3fd4
Alex Vykalyuk <alekzvik@gmail.com>
2012-04-11 15:31:58 +0300

    Added link to github mirror

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ html_theme = 'flask_small'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = dict(github_fork=False, index_logo=False)
+html_theme_options = dict(github_fork='maxcountryman/flask-login', index_logo=False)
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
