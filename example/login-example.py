ef6aaea8dcc0a36817619d489790f06d29f8b152
sederek <1151986@gmail.com>
2012-08-29 15:55:53 +0300

    added snippet code at http://flask.pocoo.org/snippets/51/ to the example provided

--- example/login-example.py
+++ example/login-example.py
@@ -13,6 +13,58 @@ from flask.ext.login import (LoginManager, current_user, login_required,
                             login_user, logout_user, UserMixin, AnonymousUser,
                             confirm_login, fresh_login_required)
 
+from werkzeug.datastructures import CallbackDict
+from flask.sessions import SessionInterface, SessionMixin
+from itsdangerous import URLSafeTimedSerializer, BadSignature
+
+
+class ItsdangerousSession(CallbackDict, SessionMixin):
+
+    def __init__(self, initial=None):
+        def on_update(self):
+            self.modified = True
+        CallbackDict.__init__(self, initial, on_update)
+        self.modified = False
+
+
+class ItsdangerousSessionInterface(SessionInterface):
+    salt = 'cookie-session'
+    session_class = ItsdangerousSession
+
+    def get_serializer(self, app):
+        if not app.secret_key:
+            return None
+        return URLSafeTimedSerializer(app.secret_key, 
+                                      salt=self.salt)
+
+    def open_session(self, app, request):
+        s = self.get_serializer(app)
+        if s is None:
+            return None
+        val = request.cookies.get(app.session_cookie_name)
+        if not val:
+            return self.session_class()
+        max_age = app.permanent_session_lifetime.total_seconds()
+        try:
+            data = s.loads(val, max_age=max_age)
+            return self.session_class(data)
+        except BadSignature:
+            return self.session_class()
+
+    def save_session(self, app, session, response):
+        domain = self.get_cookie_domain(app)
+        if not session:
+            if session.modified:
+                response.delete_cookie(app.session_cookie_name,
+                                   domain=domain)
+            return
+        expires = self.get_expiration_time(app, session)
+        val = self.get_serializer(app).dumps(dict(session))
+        response.set_cookie(app.session_cookie_name, val,
+                            expires=expires, httponly=True,
+                            domain=domain)
+
+
 class User(UserMixin):
     def __init__(self, name, id, active=True):
         self.name = name
@@ -37,6 +89,7 @@ USER_NAMES = dict((u.name, u) for u in USERS.itervalues())
 
 
 app = Flask(__name__)
+app.session_interface = ItsdangerousSessionInterface()
 
 SECRET_KEY = "yeah, not actually a secret"
 DEBUG = True
@@ -45,6 +98,7 @@ app.config.from_object(__name__)
 
 login_manager = LoginManager()
 
+login_manager.session_protection = "strong"
 login_manager.anonymous_user = Anonymous
 login_manager.login_view = "login"
 login_manager.login_message = u"Please log in to access this page."
