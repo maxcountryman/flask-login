e3718652e7d7d887c377847b0c686751ce2dd9c2
Yohan Graterol <yohangraterol92@gmail.com>
2014-08-16 19:45:26 -0500

    Added comment for the decorator function

--- flask_login.py
+++ flask_login.py
@@ -806,7 +806,15 @@ def fresh_login_required(func):
     return decorated_view
 
 
-def is_logged(redirect_to=''):
+def is_logged(redirect_to='/'):
+    '''
+        If you decorate a view with this, it will ensure that if current user is
+        logged in and calling the view, the decorator redirect to another view,
+        and if not is logged in, show the current content for that view decorated.
+
+        :param func: The view function to decorate.
+        :type func: function
+        '''
     def is_logged_decorator(func):
         @wraps(func)
         def decorated_view(*args, **kwargs):
