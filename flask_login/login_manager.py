029c976d89b8a3b728536ebeeb219b4bdde962bd
Alan <alandawkins@users.noreply.github.com>
2017-07-30 08:53:28 +0800

    Add a description into docstring.

--- flask_login/login_manager.py
+++ flask_login/login_manager.py
@@ -289,6 +289,10 @@ class LoginManager(object):
 
     def reload_user(self, user=None):
         '''
+        This set the ctx.user with the user object loaded by your customized
+        user_loader callback function, which should retrieved the user object
+        with the user_id got from session.
+
         Syntax example:
         from flask_login import LoginManager
         @login_manager.user_loader
@@ -297,7 +301,8 @@ class LoginManager(object):
             # if you use SQLAlchemy, for example:
             user_obj = User.query.get(int(user_id))
             return user_obj
-        Reason to let you define this self.user_callback:
+
+        Reason to let YOU define this self.user_callback:
             Because we won't know how/where you will load you user object.
         '''
         ctx = _request_ctx_stack.top
@@ -311,8 +316,7 @@ class LoginManager(object):
                     raise Exception(
                         "No user_loader has been installed for this "
                         "LoginManager. Add one with the "
-                        "'LoginManager.user_loader' decorator."
-                        "Refer to docstring for more information.")
+                        "'LoginManager.user_loader' decorator.")
                 user = self.user_callback(user_id)
                 if user is None:
                     ctx.user = self.anonymous_user()
