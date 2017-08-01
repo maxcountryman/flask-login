451cf24f5e6b7b04a349d5deb7c417d50f45b5ac
Alan <alandawkins@users.noreply.github.com>
2017-08-01 09:46:20 +0800

    Update login_manager.py
    Reason to delete the hint "add LoginManager.user_loader decorator...": basically, there're two groups of people who got this exception, who know how/where/why to add this callback, and others who don't. For first group, when they're notificated "user_loader" wasn't installed, they will immediately realize what's happening; for those who don't, they're more likely knowing few about this (like me), in this situation, "add ...decorator" won't help them figure this out, they need to deep into more details.
    So, the exception message for this may should be "all or nothing".

--- flask_login/login_manager.py
+++ flask_login/login_manager.py
@@ -315,8 +315,8 @@ class LoginManager(object):
                 if self.user_callback is None:
                     raise Exception(
                         "No user_loader has been installed for this "
-                        "LoginManager. Add one with the "
-                        "'LoginManager.user_loader' decorator.")
+                        "LoginManager. Refer to https://flask-login.readthedocs.io/"
+                        "en/latest/#how-it-works for more info.")
                 user = self.user_callback(user_id)
                 if user is None:
                     ctx.user = self.anonymous_user()
