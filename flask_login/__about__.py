486c456907e307e363d0c334fb2c079d4fa25d55
Max Countryman <maxc@me.com>
2017-12-02 10:34:46 +0800

    mark v0.4.1

--- flask_login/__about__.py
+++ flask_login/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'Flask-Login'
 __description__ = 'User session management for Flask'
 __url__ = 'https://github.com/maxcountryman/flask-login'
-__version_info__ = ('0', '4', '0')
+__version_info__ = ('0', '4', '1')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Matthew Frazier'
 __author_email__ = 'leafstormrush@gmail.com'
