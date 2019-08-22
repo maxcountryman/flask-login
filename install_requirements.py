7eecc6d98ccc98714e3ba867a03f96dc334ac51c
Alan Hamlett <alan.hamlett@gmail.com>
2019-08-22 10:28:00 -0700

    only compare first two version numbers with equals

--- install_requirements.py
+++ install_requirements.py
@@ -7,7 +7,7 @@ is_dev = sys.argv[1] == "dev" if len(sys.argv) > 1 else False
 
 if sys.version_info >= (3, 5):
     requirements = "requirements.txt"
-elif sys.version_info == (2, 7):
+elif sys.version_info[:2] == (2, 7):
     if is_dev:
         requirements = "py2-requirements.txt"
     else:
