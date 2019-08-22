75a72c55c5a6a42d2d7a47ff711cc11f7b66df7a
Alan Hamlett <alan.hamlett@gmail.com>
2019-08-22 10:01:45 -0700

    drop Python 2.6 support from install_requirements script

--- install_requirements.py
+++ install_requirements.py
@@ -5,15 +5,15 @@ import os
 is_dev = sys.argv[1] == "dev" if len(sys.argv) > 1 else False
 
 
-if sys.version_info >= (3, 3):
+if sys.version_info >= (3, 5):
     requirements = "requirements.txt"
-elif (2, 6) <= sys.version_info < (3, 0):
+elif sys.version_info == (2, 7):
     if is_dev:
         requirements = "py2-requirements.txt"
     else:
         requirements = "requirements.txt"
 else:
-    raise AssertionError("only support 2.6, 2.7, 3.3")
+    raise AssertionError("only support 2.7")
 
 
 if __name__ == "__main__":
