264c16f1b794f7d408f8b3ec66384d01ae03f0d1
David Lord <davidism@gmail.com>
2022-03-29 10:48:54 -0700

    update project metadata (#646)
    * add editorconfig
    
    * update gitignore
    
    * move tests to directory
    
    * render changes as markdown
    
    * latest sphinx build script
    
    * switch to setup.cfg for project metadata
    
    * reformat release workflow similar to tests

--- setup.py
+++ setup.py
@@ -1,45 +1,10 @@
 from setuptools import setup
 
-about = {}
-with open('flask_login/__about__.py') as f:
-    exec(f.read(), about)
-
-
-with open("README.md", "rt") as f:
-    readme_text = f.read()
-
-setup(name=about['__title__'],
-      version=about['__version__'],
-      url=about['__url__'],
-      license=about['__license__'],
-      author=about['__author__'],
-      author_email=about['__author_email__'],
-      description=about['__description__'],
-      long_description=readme_text,
-      long_description_content_type='text/markdown',
-      packages=['flask_login'],
-      zip_safe=False,
-      platforms='any',
-      python_requires=">=3.7",
-      install_requires=[
-          "Flask>=1.0.4",
-          "Werkzeug>=1.0.1",
-      ],
-      classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        ])
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="Flask-Login",
+    install_requires=[
+        "Flask>=1.0.4",
+        "Werkzeug>=1.0.1",
+    ],
+)
