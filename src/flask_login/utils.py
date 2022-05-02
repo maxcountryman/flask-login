498689750529cbdebf45ffc10af72f820064f18b
Immanuel <immanuel.hayden@gmail.com>
2022-05-02 05:43:10 +0200

    Fix for #662 - Better Flask 1.x detection (#667)
    * Fix #662 - Better Flask 1.x detection
    
    * Update utils.py
    
    *cough* nobody saw that
    
    * Update utils.py
    
    Fix the black complaint

--- src/flask_login/utils.py
+++ src/flask_login/utils.py
@@ -296,11 +296,12 @@ def login_required(func):
             pass
         elif not current_user.is_authenticated:
             return current_app.login_manager.unauthorized()
-        try:
-            # current_app.ensure_sync available in Flask >= 2.0
+
+        # flask 1.x compatibility
+        # current_app.ensure_sync is only available in Flask >= 2.0
+        if callable(getattr(current_app, "ensure_sync", None)):
             return current_app.ensure_sync(func)(*args, **kwargs)
-        except AttributeError:  # pragma: no cover
-            return func(*args, **kwargs)
+        return func(*args, **kwargs)
 
     return decorated_view
 
