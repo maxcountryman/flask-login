8f0aa49b6fdb661c1f4d0a5123d533485e5b1f7c
Daniel Gaspar <danielvazgaspar@gmail.com>
2023-10-30 11:10:54 +0000

    revert ABC

--- src/flask_login/session_cache.py
+++ src/flask_login/session_cache.py
@@ -1,17 +1,11 @@
-from abc import ABC
-from abc import abstractmethod
-
-
-class BaseSessionCache(ABC):
+class BaseSessionCache:
     """
     Abstract class for session cache.
     Starting with python 3.8 we can change this to a protocol.
     """
 
-    @abstractmethod
     def get(self, key):
         pass
 
-    @abstractmethod
     def set(self, key, value):
         pass
